c72959e0662018fbbe5dc965018577f8bba0e1c2
Merlijn18 <D304943@edu.curio.nl>
2024-03-07 08:41:59 +0100

    .

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -5,21 +5,21 @@ $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
 
 //verbinding maken
 require_once '../../../config/conn.php';
 //query
-$query = "INSERT TO meldingen (attractie, capaciteit, melder)
-VALUES(:attractie, :capaciteit, :melder)";
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder)
+VALUES (:attractie, :capaciteit, :melder)";
 //prepare
 $statement = $conn->prepare($query);
 //execute
 $statement->execute([
     ":attractie" => $attractie,
-    ":capaciteit" => $capaciteit,
+    ":capaciteit" => $capaciteit,   
     ":melder" => $melder,
 ]);
 
-$items = $statement->fetchAll(PDO::FETCH_ASSOC);
+header("Location: ../../../resources/views/meldingen/index.php?msg=Melding Opgeslagen");
+
 
